a9a6abb12dbdfdd19fb94bb3c7d9f09411349891
Dylan Martinez <dylmarti@c3r1p5.42perpignan.fr>
2023-11-23 18:00:37 +0100

    trses

--- include/so_long.h
+++ include/so_long.h
@@ -6,17 +6,20 @@
 /*   By: dylmarti <dylmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 13:32:54 by dylmarti          #+#    #+#             */
-/*   Updated: 2023/11/23 13:46:43 by dylmarti         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:54:23 by dylmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include "../minilibx-linux/mlx.h"
 # include <stdlib.h>
+# include <unistd.h>
 # include <stdio.h>
 # include <string.h>
-# include <unistd.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
 # include <fcntl.h>
 # include <sys/mman.h>
 # include <X11/Xlib.h>
@@ -25,5 +28,21 @@
 # include <sys/shm.h>
 # include <X11/extensions/XShm.h>
 # include <X11/XKBlib.h>
+
+typedef struct	s_data
+{
+	void	*mlx;
+	void	*win;
+	t_image caca;
+}t_data;
+
+typedef struct	s_image
+{
+	void	*image
+	char	*image_pix;
+	int		bits_per;
+	int		endian;
+	int		line_len;
+}t_image
 
 #endif
--- src/so_long.c
+++ src/so_long.c
@@ -6,13 +6,50 @@
 /*   By: dylmarti <dylmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 13:47:34 by dylmarti          #+#    #+#             */
-/*   Updated: 2023/11/23 13:55:38 by dylmarti         ###   ########.fr       */
+/*   Updated: 2023/11/23 17:58:58 by dylmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
+int	ft_stop(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+	exit(0);	
+	return (0);
+}
+
+int	ft_key_check(int key, t_data *data)
+{
+	if (key == XK_Escape)
+		ft_stop(data);
+	return (0);
+}
+
+void	init_images(t_data *data)
+{
+	mlx_xpm_file_to_image(data->mlx, "caca.xpm", NULL, NULL);
+}
+
+void	ft_display(t_data *data)
+{
+	data->mlx = mlx_init();
+	data->win = mlx_new_window(data->mlx, 1920, 1080, "so_long");
+	
+	mlx_key_hook(data->win, ft_key_check, data);
+	mlx_hook(data->win, 17, 0L, ft_stop, data);
+	
+
+	mlx_loop(data->mlx);
+}
+
 int	main(void)
 {
+	t_data	data;
+	
+	ft_display(&data);
+	
 	return (0);
 }
