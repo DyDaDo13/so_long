274093cb8d299d3bdf65ffdc83699b8273de587e
Dylan Martinez <dylmarti@c3r1p5.42perpignan.fr>
2023-11-23 16:29:14 +0100

    test

---
+++ include/so_long.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dylmarti <dylmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 13:32:54 by dylmarti          #+#    #+#             */
+/*   Updated: 2023/11/23 13:46:43 by dylmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/mman.h>
+# include <X11/Xlib.h>
+# include <X11/Xutil.h>
+# include <sys/ipc.h>
+# include <sys/shm.h>
+# include <X11/extensions/XShm.h>
+# include <X11/XKBlib.h>
+
+#endif
